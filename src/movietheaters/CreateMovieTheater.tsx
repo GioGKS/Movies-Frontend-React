3ee012253346488185d26a060375033f1eaa9efd
GioGKS <gulikashvili205@gmail.com>
2023-06-03 14:54:08 +0300

    Updated Theaters Form

--- src/movietheaters/CreateMovieTheater.tsx
+++ src/movietheaters/CreateMovieTheater.tsx
@@ -1,7 +1,10 @@
+import MovieThaterForm from "./MovieTheaterForm";
+
 export default function CreateMovieTheater() {
     return (
       <>
         <h3>Create Movie Theaters</h3>
+        <MovieThaterForm model={{name: ''}} onSubmit={values => console.log(values)}/>
       </>
     );
   }
---
+++ src/movietheaters/MovieTheaterForm.tsx
@@ -0,0 +1,32 @@
+import { Form, Formik, FormikHelpers } from "formik";
+import { Link } from "react-router-dom";
+import TextField from "../forms/TextField";
+import Button from "../utils/Button";
+import { movieTheaterCreationDTO } from "./movieThater.model";
+import * as Yup from 'yup'
+
+export default function MovieThaterForm(props: movieTheaterForm){
+    return (
+        <Formik 
+        initialValues={props.model}
+        onSubmit={props.onSubmit}
+        validationSchema={Yup.object({
+            name: Yup.string().required('This field is required').firstLetterUppercase()
+        })}
+        >
+            {(formikProps) => (
+                <Form>
+                    <TextField displayName="Name" field="name"/>
+
+                    <Button disabled={formikProps.isSubmitting} type="submit">Save Changes</Button>
+                    <Link className="btn btn-secondary" to="/movietheaters">cancel</Link>
+                </Form>
+            )}
+        </Formik>
+    )
+}
+
+interface movieTheaterForm{
+    model: movieTheaterCreationDTO;
+    onSubmit(values: movieTheaterCreationDTO, actions: FormikHelpers<movieTheaterCreationDTO>): void;
+}
