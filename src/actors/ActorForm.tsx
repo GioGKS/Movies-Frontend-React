d760174bfbf03f56db920e628f340663915d39ad
GioGKS <gulikashvili205@gmail.com>
2023-06-03 13:57:24 +0300

    Select Picture Of Actor Component

--- src/actors/ActorForm.tsx
+++ src/actors/ActorForm.tsx
@@ -4,15 +4,20 @@ import TextField from "../forms/TextField";
 import Button from "../utils/Button";
 import { actorCreationDTO } from "./actors.mode";
 import * as Yup from 'yup';
+import DateField from "../forms/DateField";
+import ImageField from "../forms/ImageField";
 
 export default function ActorForm(props: actorFormProps) {
   return (
     <Formik initialValues={props.model} onSubmit={props.onSubmit} validationSchema={Yup.object({
-        name: Yup.string().required('This field is required').firstLetterUppercase()
+        name: Yup.string().required('This field is required').firstLetterUppercase(),
+        dateOfBirth: Yup.date().nullable().required('This field is required')
     })}>
       {(formikProps) => (
         <Form>
           <TextField displayName="Name" field="name" />
+          <DateField displayName="Date of Birth" field="dateOfBirth"/>
+          <ImageField displayName="Picture" field="picture" imageURL={props.model.pictureURL}/>
 
           <Button disabled={formikProps.isSubmitting} type="submit">
             Save Changes
