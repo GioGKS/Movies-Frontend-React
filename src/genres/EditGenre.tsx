f6f9b1be53ac3fa08a0516c54a47b21682dc12d3
GioGKS <gulikashvili205@gmail.com>
2023-06-21 23:36:11 +0300

    edit entity

--- src/genres/EditGenre.tsx
+++ src/genres/EditGenre.tsx
@@ -3,50 +3,29 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { urlGenres } from "../endpoints";
 import DisplayErrors from "../utils/DisplayErrors";
+import EditEntity from "../utils/EditEntity";
 import Loading from "../utils/Loading";
 import GenreForm from "./GenreForm";
 import { genreCreationDTO, genreDTO } from "./genres.mode";
 
 export default function EditGenre() {
-  const { id }: any = useParams();
-  const [genre, setGenre] = useState<genreCreationDTO>();
-  const [errors, setErrors] = useState<string[]>([]);
-  const history = useNavigate();
-
-  useEffect(() => {
-    axios
-      .get(`${urlGenres}/${id}`)
-      .then((response: AxiosResponse<genreDTO>) => {
-        setGenre(response.data);
-      });
-  }, [id]);
-
-  async function edit(genreToEdit: genreCreationDTO) {
-    try {
-      await axios.put(`${urlGenres}/${id}`, genreToEdit);
-      history('/genres');
-    } catch (error: any) {
-      if (error && error.response) {
-        setErrors(error.response.data);
-      }
-    }
-  }
+  
 
   return (
     <>
-      <h3>Edit Genre</h3>
-      <DisplayErrors errors={errors}></DisplayErrors>
-      {genre ? (
-        <GenreForm
-          model={genre}
+      <EditEntity<genreCreationDTO, genreDTO>
+        url={urlGenres} entityName="Genres"
+      >
+        {(entity, edit) => 
+          <GenreForm
+          model={entity}
           onSubmit={async (value) => {
             // when the form is posted
             await edit(value);
           }}
         />
-      ) : (
-        <Loading />
-      )}
+        }
+      </EditEntity>
     </>
   );
 }
---
+++ src/utils/EditEntity.tsx
@@ -0,0 +1,54 @@
+import axios, { AxiosResponse } from "axios";
+import { ReactElement, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import GenreForm from "../genres/GenreForm";
+import { genreCreationDTO } from "../genres/genres.mode";
+import DisplayErrors from "./DisplayErrors";
+import Loading from "./Loading";
+
+export default function EditEntity<TCreation, TRead>(
+  props: editEntityProps<TCreation, TRead>
+) {
+  const { id }: any = useParams();
+  const [entity, setEntity] = useState<TCreation>();
+  const [errors, setErrors] = useState<string[]>([]);
+  const history = useNavigate();
+
+  useEffect(() => {
+    axios.get(`${props.url}/${id}`).then((response: AxiosResponse<TRead>) => {
+      setEntity(props.transform(response.data));
+    });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  async function edit(entityToEdit: TCreation) {
+    try {
+      await axios.put(`${props.url}/${id}`, entityToEdit);
+      history(props.indexURL);
+    } catch (error: any) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
+  }
+
+  return (
+    <>
+      <h3>Edit {props.entityName}</h3>
+      <DisplayErrors errors={errors}></DisplayErrors>
+      {entity ? props.children(entity, edit) : <Loading />}
+    </>
+  );
+}
+
+interface editEntityProps<TCreation, TRead> {
+  url: string;
+  entityName: string;
+  indexURL: string;
+  transform(entity: TRead): TCreation;
+  children(entity: TCreation, edit: (entity: TCreation) => void): ReactElement;
+}
+
+EditEntity.defaultProps = {
+  transform: (entity: any) => entity,
+};
