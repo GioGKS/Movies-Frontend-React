549edb4377fa780a32bba6b8fbe00ef345930ea0
GioGKS <gulikashvili205@gmail.com>
2023-06-23 21:24:38 +0300

    posting actor

--- src/actors/CreateActor.tsx
+++ src/actors/CreateActor.tsx
@@ -1,13 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { urlActors } from "../endpoints";
+import DisplayErrors from "../utils/DisplayErrors";
+import convertActorToFormData from "../utils/formDataUtils";
 import ActorForm from "./ActorForm";
+import { actorCreationDTO } from "./actors.mode";
 
 export default function CreateActor() {
-    return (
-      <>
-        <h3>Create Actor</h3>
-        <ActorForm model={{name: '', dateOfBirth: undefined}} 
-          onSubmit={values => console.log(values)}
-        />
-      </>
-    );
+  const [errors, setErrors] = useState<string[]>([]);
+  const history = useNavigate();
+
+  async function create(actor: actorCreationDTO) {
+    try {
+      const formData = convertActorToFormData(actor);
+
+      await axios({
+        method: 'post',
+        url: urlActors,
+        data: formData,
+        headers: {'Content-Type': 'multipart/form-data'}
+      });
+      history('/actors');
+
+    } catch (error: any) {
+      if (error && error.response) {
+        setErrors(error.response.data);
+      }
+    }
   }
-  
+
+  return (
+    <>
+      <h3>Create Actor</h3>
+      <DisplayErrors errors={errors} />
+      <ActorForm
+        model={{ name: "", dateOfBirth: undefined }}
+        onSubmit={async values => await create(values)}
+      />
+    </>
+  );
+}
