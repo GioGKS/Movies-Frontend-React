aa58826418a55ef3d7f1d6d27def86b6c72c5c62
GioGKS <gulikashvili205@gmail.com>
2023-06-30 17:48:37 +0300

    added claims

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
-import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Menu from "./Menu";
 import routes from "./route-config";
-import configureValidations from './Validations';
+import configureValidations from "./Validations";
+import { claim } from "./auth/auth.models";
+import { useState } from "react";
+import AuthenticationContext from "./auth/AuthenticationContext";
 
 configureValidations();
 
 function App() {
+  const [claims, setClaims] = useState<claim[]>([
+    {name: 'email', value: 'gio97@gmail.com'}
+  ]);
+
   return (
     <BrowserRouter>
-      <Menu />
-      <div className="container">
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          ))}
-        </Routes>
-      </div>
-      <footer className="bd-footer py-4 mt-4 bg-light">
+      <AuthenticationContext.Provider value={{ claims, update: setClaims }}>
+        <Menu />
         <div className="container">
-            Copyrights By GIO GKS {new Date().getFullYear().toString()}
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.component />}
+              />
+            ))}
+          </Routes>
         </div>
-
-      </footer>
+        <footer className="bd-footer py-4 mt-4 bg-light">
+          <div className="container">
+            Copyrights By GIO GKS {new Date().getFullYear().toString()}
+          </div>
+        </footer>
+      </AuthenticationContext.Provider>
     </BrowserRouter>
   );
 }
